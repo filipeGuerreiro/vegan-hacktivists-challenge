93fcc199444ce408ea77c9305bcf6b31a5272af2
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2021-06-09 13:42:35 +0900

    Add route paths for get and posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Answer;
+use App\Models\Question;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('questions', [
+        'questions' => Question::all()
+    ]);
+});
+
+Route::post('questions', function (Request $request) {
+    // TODO probably put this code into service...?
+    Question::create([
+        'title' => $request->content,
+        'answers' => 0
+    ]);
+
+    return redirect('/');
+});
+
+Route::get('questions/{id}', function ($id) {
+    return view('question', [
+        'question_id' => $id,
+        'answers' => Question::findOrFail($id)->answers()
+    ]);
+})->whereNumber('id');
+
+Route::post('questions/{id}', function ($id, Request $request) {
+    Answer::create([
+        'question_id' => $id,
+        'content' => $request->content
+    ]);
+
+    return redirect('questions/{$id}');
 });
